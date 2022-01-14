2ab61022f4bd9b2df6e3b3ed708c2ab548440a86
PauloCanez <97709930+PauloCanez@users.noreply.github.com>
2022-01-14 15:44:28 -0800

    Add files via upload

---
+++ tests/testthat/test_nothing.R
@@ -0,0 +1,21 @@
+describe("Test all is ready", {
+  it("return one", {
+    expected <- 1
+    obtained <- return_one()
+    expect_equal(expected, obtained)
+  })
+  it("return two", {
+    expected <- 2
+    obtained <- return_two()
+    expect_equal(expected, obtained)
+  })
+})
+
+describe("Get version of the module", {
+  it("The version is 0.1.0", {
+    expected_version <- c("0.1.0")
+    obtained_version <- packageVersion("SeleccionPvvc2022")
+    version_are_equal <- expected_version == obtained_version
+    expect_true(version_are_equal)
+  })
+})
